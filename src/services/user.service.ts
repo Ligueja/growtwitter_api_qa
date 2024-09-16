7d5c282bb3f87b39eb059db5e7fdb0e7be67b8cf
Luiz Coelho <ligueja@hotmail.com>
2024-09-16 16:56:12 -0300

    refactor list users

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,40 +1,85 @@
 // o seervice contém regras de negócio e chamadas no BD.
-
 import { User } from "@prisma/client";
 import { prismaConnection } from "../database/prisma.connection";
 import { Bcrypt } from "../libs/bcrypt.lib";
-import { CreateUserDTO } from "../dtos";
+import {
+  CreateUserDTO,
+  ListAllUsersInputDTO,
+  ListallUsersOutputDTO,
+} from "../dtos";
 import { HttpError } from "../erros/http.error";
 
 export class UserService {
-  // no imput é a entrada de dados na promise é a saida de dados
-  public async createUser(imput: CreateUserDTO): Promise<User> {
-    const existingEmail = await this.existingEmail(imput.email);
+  // no input é a entrada de dados na promise é a saida de dados
+  public async createUser(input: CreateUserDTO): Promise<User> {
+    const existingEmail = await this.existingEmail(input.email);
 
     if (existingEmail) {
       throw new HttpError("E-mail já cadastrado", 409);
     }
 
-    const existingUsername = await this.existingUsername(imput.username);
+    const existingUsername = await this.existingUsername(input.username);
 
     if (existingUsername) {
       throw new HttpError("Username já cadastrado", 409);
     }
 
     const bcrypt = new Bcrypt();
-    const hash = await bcrypt.encoded(imput.password);
+    const hash = await bcrypt.encoded(input.password);
 
     const newUser = await prismaConnection.user.create({
       data: {
-        name: imput.name,
-        email: imput.email,
-        username: imput.username,
+        name: input.name,
+        email: input.email,
+        username: input.username,
         password: hash,
-        avatar: imput.avatar,
+        avatar: input.avatar,
       },
     });
 
     return newUser;
+  }
+
+  public async listUsers(
+    input: ListAllUsersInputDTO
+  ): Promise<ListallUsersOutputDTO> {
+    let limitDefault = 5;
+    let pageDefault = 1;
+
+    if (input.limit) {
+      limitDefault = Number(input.limit);
+    }
+
+    if (input.page) {
+      pageDefault = Number(input.page);
+    }
+
+    const totalUsers = await this.totalUsers();
+
+    if (totalUsers === 0) {
+      throw new HttpError("Nenhum usuário cadastrado", 404);
+    }
+
+    const users = await prismaConnection.user.findMany({
+      skip: limitDefault * (pageDefault - 1),
+      take: limitDefault,
+      orderBy: {
+        name: "asc",
+      },
+      where: {
+        deleted: false,
+      },
+    });
+
+    return {
+      data: users,
+      pagination: {
+        limit: limitDefault,
+        page: pageDefault,
+        count: totalUsers,
+        totalPages: Math.ceil(totalUsers / limitDefault),
+      },
+    };
   }
 
   public async existingEmail(email: any): Promise<boolean> {
@@ -59,5 +104,15 @@ export class UserService {
 
     // Boolean(existingUsername) transforma o objeto "existingEmail em boolean"
     return Boolean(existingUsername);
+  }
+
+  public async totalUsers(): Promise<number> {
+    const totalUsers = await prismaConnection.user.count({
+      where: {
+        deleted: false,
+      },
+    });
+
+    return totalUsers;
   }
 }
